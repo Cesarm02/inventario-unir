69c1de9dcf339a2642ec1e9abf813ed6866f60e3
Cesar Estiven Medrano <cesar-2007-mesa@hotmail.com>
2022-12-27 17:30:04 -0500

    Router basico

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navegacion from "./Layouts/Navegacion";
+import Sigin from "./pages/Signin";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <>
+          <Navegacion></Navegacion>   
+      </>
+      <Container>
+        <Routes> 
+            <Route exact path="/" element = {<Home/>}></Route>
+            <Route exact path="/signin" element={<Sigin></Sigin>} ></Route>
+            <Route path="*" element = {<NotFound/>}></Route>
+        </Routes>
+      </Container>
+      
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/Layouts/Navegacion.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom';
+
+export default function Navegacion() {
+  return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand as={NavLink} to={'/'}>Inventario Unir</Navbar.Brand>
+            <Navbar.Toggle aria-controls='main-menu'>
+
+            </Navbar.Toggle>
+            <Navbar.Collapse id="main-menu">
+                <Nav className="mr-auto">
+                    <NavDropdown title="Productos" id="menu-dropdown">
+                        <NavDropdown.Item>Categorias</NavDropdown.Item>
+                        <NavDropdown.Item>Lista</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Personas" id="menu-dropdown">
+                        <NavDropdown.Item>Clientes</NavDropdown.Item>
+                        <NavDropdown.Item>Proveedores</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Tienda" id="menu-dropdown">
+                        <NavDropdown.Item>Venta</NavDropdown.Item>
+                        <NavDropdown.Item>Compra</NavDropdown.Item>
+                        <NavDropdown.Item>Cotización</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Acciones" id="menu-dropdown">
+                        <NavDropdown.Item>Inventario</NavDropdown.Item>
+                        <NavDropdown.Item>Reportes</NavDropdown.Item>
+                        <NavDropdown.Item>Ajustes</NavDropdown.Item>
+                    </NavDropdown>
+
+                </Nav>
+                <Nav>
+                    <Nav.Link>Crear Cuenta</Nav.Link>
+                    <Nav.Link as={NavLink} to={'/signin'}>Iniciar sesion</Nav.Link>
+                    <NavDropdown title="Cesar Mesa" id="menu-dropdown">
+                        <NavDropdown.Item>Cerrar sesion</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
